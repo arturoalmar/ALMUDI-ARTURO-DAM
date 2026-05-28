e715cc6bf23a0ddd517a345051c25b99801fc93b
arturoalmar <a29310@svalero.com>
2026-05-28 10:38:58 +0200

    Add files via upload

---
+++ DAM2_Almudi_Arturo_JDBC/DAM2_Almudi_Arturo_JDBC/src/examen/Almudi/Arturo/DAO/SateliteDAO.java
@@ -0,0 +1,31 @@
+package examen.Almudi.Arturo.DAO;
+
+import examen.Almudi.Arturo.beans.Satelite;
+
+import java.util.ArrayList;
+public class SateliteDAO implements DAO<Satelite>{
+    @Override
+    public void add(Satelite object) {
+
+    }
+
+    @Override
+    public void update(int id, Satelite object) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public Satelite find(int id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Satelite> findAll() {
+        return null;
+    }
+}
